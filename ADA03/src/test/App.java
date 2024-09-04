2c91a078032b3db7a0d40c4a40b588b3d3628e69
Alexandercp7 <A22216350@alumnos.uady.mx>
2024-09-04 15:25:49 -0600

    txt

--- ADA03/src/test/App.java
+++ ADA03/src/test/App.java
@@ -10,7 +10,8 @@ public class App {
     public static void main(String[] args) {
         ArrayList<String> fileNames = new ArrayList<>();
         ArrayList<String> formatedNames = new ArrayList<>();
-        CustomFileReader reader = new CustomFileReader("nombres.txt");
+        CustomFileReader reader = new CustomFileReader("C:\\Users\\eacan\\Documents\\Pers\\arquitectura-software\\ADA03\\n" + //
+                        "ombres.txt");
         fileNames = reader.read();
         TextFormatter formatter = new TextFormatter(fileNames);
         formatedNames = formatter.formatText();
